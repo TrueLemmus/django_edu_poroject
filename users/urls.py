795fc8e4ec70421be26e923ae314fd5f47342f53
TrueLemmus <sarekd@gmail.com>
2021-08-23 19:59:05 +0300

    users/urls.py fixed

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from users.views import login, registration, logout
 app_name = 'users'
 
 urlpatterns = [
-    path('login', login, name='login'),
-    path('registration', registration, name='registration'),
-    path('logout', logout, name='logout')
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration'),
+    path('logout/', logout, name='logout')
 ]
