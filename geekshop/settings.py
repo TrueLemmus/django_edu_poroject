54aef4484f6a3f4ff072e4005429b97a3fddafa2
TrueLemmus <sarekd@gmail.com>
2021-08-28 12:24:12 +0300

    added login_required

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.user'
+
+LOGIN_URL = '/users/login/'
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from baskets.models import Basket
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
@@ -39,6 +40,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     user = request.user
     if request.method == 'POST':
