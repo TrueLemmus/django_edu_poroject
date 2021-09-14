49694392ac713589cb96a6a9cc2119fa9c81666c
TrueLemmus <sarekd@gmail.com>
2021-09-14 22:54:42 +0300

    added user email activation

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
+import random, hashlib
 
 from users.models import User
 
@@ -36,6 +37,16 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def save(self):
+        user = super(UserRegistrationForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        user.save()
+
+        return user
 
 
 class UserProfileForm (UserChangeForm):
--- users/views.py
+++ users/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, HttpResponseRedirect
-
+from django.core.mail import send_mail
+from django.conf import settings
 from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from baskets.models import Basket
 from django.contrib.auth.decorators import login_required
+from users.models import User
+
+
+def send_verify_mail(user):
+    verify_link = reverse('users:verify', args=[user.email, user.activation_key])
+
+    title = f'Подтверждение учетной записи {user.username}'
+
+    message = f'Для подтверждения учетной записи {user.username} на портале' \
+              f'{settings.DOMAIN_NAME} перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
+
+    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, activation_key):
+    try:
+        user = User.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            print(f'User {user} activated')
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            print(f'error activation user: {user}')
+            return HttpResponseRedirect(reverse('index'))
+    except Exception as e:
+        print(f'error activation user : {e.args}')
+        return HttpResponseRedirect(reverse('index'))
 
 
 def login(request):
@@ -31,9 +61,14 @@ def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Вы успешно зарегестрировались!')
-            return HttpResponseRedirect(reverse('users:login'))
+            user = form.save()
+            messages.success(request, 'Для активации аккаунта пройдите по ссылке на вашей почте.')
+            if send_verify_mail(user):
+                print('сообщение подтверждения отправлено')
+                return HttpResponseRedirect(reverse('users:login'))
+            else:
+                print('ошибка отправки сообщения')
+                return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
     context = {'title': 'GeekShop - Регистрация', 'form': form}
